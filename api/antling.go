e78766d0b41fc6b363f7267072da7381f7366e24
Maxime Vidori <maxime.vidori@gmail.com>
2017-05-06 21:15:45 +0200

    Add a listing antling endpoint

--- api/antling.go
+++ api/antling.go
@@ -10,17 +10,57 @@ type Antling struct {
 	Id int `json:"id"`
 }
 
+type Antlings struct {
+	Antlings []*Antling `json:"antlings"`
+}
+
 func antlingPost(w http.ResponseWriter, r *http.Request) {
-	query := `
-    INSERT INTO anthive.antling
-    DEFAULT VALUES
-    RETURNING anthive.antling.id
-  `
+	var err error
+	query := "INSERT INTO anthive.antling "
+	query += "DEFAULT VALUES "
+	query += "RETURNING anthive.antling.id"
+
 	a := &Antling{}
-	db.Conn.QueryRow(query).Scan(&a.Id)
-	common.Encode(w, a)
+	err = db.Conn.QueryRow(query).Scan(&a.Id)
+	if err != nil {
+		common.Error.Println(err)
+		return
+	}
+	err = common.Encode(w, a)
+	if err != nil {
+		common.Error.Println(err)
+		return
+	}
+}
+
+func antlingGet(w http.ResponseWriter, r *http.Request) {
+	var err error
+	query := "SELECT anthive.antling.id "
+	query += "FROM anthive.antling"
+	rows, err := db.Conn.Query(query)
+	if err != nil {
+		common.Error.Println(err)
+		return
+	}
+	defer rows.Close()
+	antlings := []*Antling{}
+	for rows.Next() {
+		antling := &Antling{}
+		err = rows.Scan(&antling.Id)
+		if err != nil {
+			common.Error.Println(err)
+			return
+		}
+		antlings = append(antlings, antling)
+	}
+	err = common.Encode(w, Antlings{antlings})
+	if err != nil {
+		common.Error.Println(err)
+		return
+	}
 }
 
 func init() {
-	Router.HandleFunc("/antling", antlingPost).Methods("POST")
+	Router.HandleFunc("/antlings", antlingPost).Methods("POST")
+	Router.HandleFunc("/antlings", antlingGet).Methods("GET")
 }
