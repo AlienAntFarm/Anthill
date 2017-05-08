d12fc3595135c25ca43abe9e03aabab7918c1f59
Maxime Vidori <maxime.vidori@gmail.com>
2017-05-08 23:45:20 +0200

    Move anthive to cobra

--- anthive.go
+++ anthive.go
@@ -6,34 +6,47 @@ import (
 	"github.com/alienantfarm/anthive/api"
 	"github.com/alienantfarm/anthive/utils"
 	"github.com/golang/glog"
+	"github.com/spf13/cobra"
 	"net/http"
 	"os"
 	"time"
 )
 
+var rootCmd = &cobra.Command{
+	Use:   "anthive",
+	Short: "Start anthive server",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// reinit args for glog
+		os.Args = os.Args[:1]
+		flag.Set("logtostderr", "true")
+		flag.Parse()
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		conf := utils.Config()
+		if conf.Debug {
+			flag.Set("v", "10") // totally arbitrary but who cares!
+			flag.Parse()
+		}
+		glog.V(1).Infoln("debug mode enabled")
+		api.InitScheduler()
+
+		addr := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
+
+		s := &http.Server{
+			Addr:           addr,
+			Handler:        api.Router,
+			MaxHeaderBytes: 1 << 20,
+			ReadTimeout:    5 * time.Second,
+			WriteTimeout:   5 * time.Second,
+		}
+
+		glog.Infof("running on http://%s/\n", addr)
+		glog.Errorf("%s", s.ListenAndServe())
+	},
+}
+
 func main() {
-	// reinit args for glog
-	os.Args = os.Args[:1]
-	conf := utils.Config()
-
-	flag.Set("logtostderr", "true")
-	if conf.Debug {
-		flag.Set("v", "10") // totally arbitrary but who cares!
+	if err := rootCmd.Execute(); err != nil {
+		glog.Fatalf("%s", err)
 	}
-	flag.Parse()
-	glog.V(1).Infoln("debug mode enabled")
-	api.InitScheduler()
-
-	addr := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
-
-	s := &http.Server{
-		Addr:           addr,
-		Handler:        api.Router,
-		MaxHeaderBytes: 1 << 20,
-		ReadTimeout:    5 * time.Second,
-		WriteTimeout:   5 * time.Second,
-	}
-
-	glog.Infof("running on http://%s/\n", addr)
-	glog.Errorf("%s", s.ListenAndServe())
 }
