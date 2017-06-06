9c8072aa8fdc5d855917502969dcb14e455d431b
Maxime Vidori <maxime.vidori@gmail.com>
2017-06-06 19:41:56 +0200

    Add util functions around job struct

--- utils/structs/structs.go
+++ utils/structs/structs.go
@@ -64,6 +64,37 @@ type Job struct {
 	Image     Image    `json:"image"`
 }
 
+func (j *Job) SanitizeCwd() string {
+	if j.Cwd != "" {
+		return j.Cwd
+	}
+	if j.Image.Cwd != "" {
+		return j.Image.Cwd
+	}
+	return "/"
+}
+
+func (j *Job) SanitizeEnv() []string {
+	env := j.Image.Env
+	if j.Image.Env == nil {
+		env = []string{}
+	}
+	if j.Env != nil {
+		env = append(env, j.Env...)
+	}
+	return env
+}
+
+func (j *Job) SanitizeCmd() []string {
+	if j.Cmd != nil {
+		return j.Cmd
+	}
+	if j.Image.Cmd != nil {
+		return j.Image.Cmd
+	}
+	return []string{"cat", "/etc/hostname"}
+}
+
 type Jobs struct {
 	Jobs []*Job `json:"jobs"`
 }
